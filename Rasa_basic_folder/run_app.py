61565cafa313bf919c50e6b08c9d8cd0e91d46b6
Goutam Dutta <42313857+duttagoutam@users.noreply.github.com>
2019-01-26 21:02:50 +0530

    Update run_app.py

--- Rasa_basic_folder/run_app.py
+++ Rasa_basic_folder/run_app.py
@@ -7,9 +7,9 @@ from rasa_slack_connector import SlackInput
 nlu_interpreter = RasaNLUInterpreter('./models/nlu/default/restaurantnlu')
 agent = Agent.load('./models/dialogue', interpreter = nlu_interpreter)
 
-input_channel = SlackInput('xoxp-455545604819-456694959687-456937338551-fd82f8366e835c911a7dad05df8bd26b', #app verification token
-							'xoxb-455545604819-455274087745-yZAhjiiLWrrihtCLNq5sR6ul', # bot verification token
-							'X1R7CUdbr2xrFQYlSk0u0hqz', # slack verification token
+input_channel = SlackInput('xoxp-455545604819-456694959687-456937338551-xxxxxx', #app verification token
+							'xoxb-455545604819-455274087745-xxxxxx', # bot verification token
+							'X1R7CUdbr2xrFQYxxxxxx', # slack verification token
 							True)
 
 agent.handle_channels([input_channel],5004, True)
